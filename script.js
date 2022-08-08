e8d736e934dec2e4faf6d818c41bb1ed6bb0abc9
Under <matheusmarianoe@gmail.com>
2022-08-08 17:34:25 -0300

    Fixed Progress Bar Indicator on mobile

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  var scrolled = (winScroll / height) * 200;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 function mudaTema() {
